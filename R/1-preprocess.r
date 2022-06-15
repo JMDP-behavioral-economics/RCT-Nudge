1a217663bedcb868a12c93dce91bd011e6d9e58c
KatoPachi <hkato.econ@gmail.com>
2022-06-15 20:45:51 +0900

    read raw data

--- R/1-preprocess.r
+++ R/1-preprocess.r
@@ -1,3 +1,19 @@
 #+ include = FALSE
 library(here)
 source(here("R", "_library.r"))
+
+#+ include = FALSE
+root <- "D:/JMDPフィールド実験"
+raw <- "raw.csv"
+schedule <- "schedule.csv"
+
+#+ include = FALSE
+rawdt <- read_csv(
+  here(root, "original", raw),
+  locale = locale(encoding = "cp932")
+)
+
+schedule_dt <- read_csv(
+  here(root, "original", schedule),
+  locale = locale(encoding = "cp932")
+)
