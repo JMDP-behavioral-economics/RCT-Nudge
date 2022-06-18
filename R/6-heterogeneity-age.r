b8aab6a73a1a513e9a7663df20c412c8251e7833
KatoPachi <63113684+KatoPachi@users.noreply.github.com>
2022-06-18 22:01:11 +0900

    add plot of age heterogenous effect

--- R/6-heterogeneity-age.r
+++ R/6-heterogeneity-age.r
@@ -48,46 +48,100 @@ est <- use %>%
     male,
     age
   ) %>%
+  dplyr::filter(prefecture != "海外") %>%
+  mutate(prefecture = droplevels(prefecture)) %>%
+  mutate(age_group = ntile(age, 4)) %>%
+  group_by(age_group) %>%
+  mutate(
+    min_age = min(age),
+    max_age = max(age)
+  ) %>%
+  ungroup() %>%
   pivot_longer(reply:donate, names_to = "outcome", values_to = "value") %>%
   mutate(outcome = factor(outcome, out_lev, out_lab)) %>%
-  group_by(outcome) %>%
-  do(est = feols(
-    value ~ treat + age + male + prefecture | month + week,
-    cluster = ~ week,
-    data = .
-  ))
+  group_by(outcome, age_group) %>%
+  do(
+    est = feols(
+      value ~ treat + male + prefecture | month + week,
+      cluster = ~ week,
+      data = .
+    ),
+    min = unique(.$min_age),
+    max = unique(.$max_age)
+  )
 
 testBC <- lapply(est$est, function(x) lincom_fixest(
-  matrix(c(1, -1, 0, rep(0, 49)), ncol = 1), x
+  matrix(c(1, -1, 0, rep(0, 47)), ncol = 1), x
 )) %>% as_vector() %>% sprintf("%1.3f", .)
 
 testBD <- lapply(est$est, function(x) lincom_fixest(
-  matrix(c(1, 0, -1, rep(0, 49)), ncol = 1), x
+  matrix(c(1, 0, -1, rep(0, 47)), ncol = 1), x
 )) %>% as_vector() %>% sprintf("%1.3f", .)
 
 testCD <- lapply(est$est, function(x) lincom_fixest(
-  matrix(c(0, 1, -1, rep(0, 49)), ncol = 1), x
+  matrix(c(0, 1, -1, rep(0, 47)), ncol = 1), x
 )) %>% as_vector() %>% sprintf("%1.3f", .)
 
 #+
-est %>%
-  pull(est, name = outcome) %>%
-  modelsummary(
-    stars = c("*" = 0.1, "**" = 0.05, "***" = 0.01),
-    coef_map = c(
-      "treatB" = "Treatment B",
-      "treatC" = "Treatment C",
-      "treatD" = "Treatment D"
-    ),
-    gof_omit = "R2 Adj.|R2 Within|R2 Pseudo|AIC|BIC|Log|Std|FE",
-    add_rows = tribble(
-      ~terms, ~"(1)", ~"(2)", ~"(3)", ~"(4)", ~"(5)", ~"(6)",
-      "Male dummy, age, prefecture dummies", "X", "X", "X", "X", "X", "X",
-      "Week and month fixed effect", "X", "X", "X", "X", "X", "X"
-    ) %>%
-    rbind(c("B = C", testBC)) %>%
-    rbind(c("B = D", testBD)) %>%
-    rbind(c("C = D", testCD))
+plotdt <- est %>%
+  summarize(
+    outcome = outcome,
+    age = age_group,
+    coef_B = coef(est)["treatB"],
+    coef_C = coef(est)["treatC"],
+    coef_D = coef(est)["treatD"],
+    se_B = est$coeftable["treatB", 2],
+    se_C = est$coeftable["treatC", 2],
+    se_D = est$coeftable["treatD", 2],
+    lwr.coef_B = coef_B - se_B * abs(qt(0.025, df = df)),
+    lwr.coef_C = coef_C - se_C * abs(qt(0.025, df = df)),
+    lwr.coef_D = coef_D - se_D * abs(qt(0.025, df = df)),
+    upr.coef_B = coef_B + se_B * abs(qt(0.025, df = df)),
+    upr.coef_C = coef_C + se_C * abs(qt(0.025, df = df)),
+    upr.coef_D = coef_D + se_D * abs(qt(0.025, df = df))
   ) %>%
-  kableExtra::kable_styling() %>%
-  kableExtra::group_rows("F-tests, p-value", 11, 13, bold = FALSE, italic = TRUE)
+  tidyr::pivot_longer(
+    coef_B:upr.coef_D,
+    names_to = c("stats", "treat"),
+    names_pattern = "(.*)_(.)"
+  ) %>%
+  tidyr::pivot_wider(names_from = stats, values_from = value) %>%
+  mutate(
+    treat = factor(treat, levels = LETTERS[2:4])
+  )
+
+scale_x_dt <- est %>%
+  select(age_group, min, max) %>%
+  distinct() %>%
+  group_by(age_group) %>%
+  summarize(
+    min = min[[1]] + median(rawdt$age),
+    max = max[[1]] + median(rawdt$age),
+    label = paste0("[", min, ", ", max, "]")
+  )
+
+#+
+plotdt %>%
+  dplyr::filter(outcome %in% out_lab[1:3]) %>%
+  ggplot(aes(x = age, y = coef)) +
+  geom_hline(aes(yintercept = 0), linetype = 2) +
+  geom_pointrange(aes(ymin = lwr.coef, ymax = upr.coef)) +
+  geom_line() +
+  scale_x_continuous(labels = scale_x_dt$label) +
+  facet_grid(treat ~ outcome) +
+  labs(x = "Age group", y = "Estimated treatment effect") +
+  simplegg() +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
+
+#+
+plotdt %>%
+  dplyr::filter(!(outcome %in% out_lab[1:3])) %>%
+  ggplot(aes(x = age, y = coef)) +
+  geom_hline(aes(yintercept = 0), linetype = 2) +
+  geom_pointrange(aes(ymin = lwr.coef, ymax = upr.coef)) +
+  geom_line() +
+  scale_x_continuous(labels = scale_x_dt$label) +
+  facet_grid(treat ~ outcome) +
+  labs(x = "Age group", y = "Estimated treatment effect") +
+  simplegg() +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
