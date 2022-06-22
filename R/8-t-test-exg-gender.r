bb30114c35e1ff8d03011a153a925ab9c9d34e74
KatoPachi <hkato.econ@gmail.com>
2022-06-22 22:22:56 +0900

    add t.test among males and females w/o exg stop - #3

--- R/8-t-test-exg-gender.r
+++ R/8-t-test-exg-gender.r
@@ -32,6 +32,7 @@ ttest_info <- expand.grid(
   outcome = out_lev,
   group1 = LETTERS[1:4],
   group2 = LETTERS[1:4],
+  male = c(1, 0),
   stringsAsFactors = FALSE) %>%
   dplyr::filter(group1 < group2) %>%
   arrange(outcome) %>%
@@ -40,7 +41,7 @@ ttest_info <- expand.grid(
 
 #+
 outcomes <- use %>%
-  select(id, treat, reply, intention:donate) %>%
+  select(id, male, treat, reply, intention:donate) %>%
   pivot_longer(reply:donate, names_to = "outcome")
 
 exclude <- use %>%
@@ -56,7 +57,7 @@ testdt <- outcomes %>%
 #+
 stat <- testdt %>%
   dplyr::filter(exclude == 0) %>%
-  group_by(outcome, treat) %>%
+  group_by(outcome, male, treat) %>%
   summarize(mean = mean(value), se = se(value)) %>%
   ungroup() %>%
   mutate(
@@ -69,10 +70,14 @@ ttest <- ttest_info %>%
   group_by(id) %>%
   do(test = t.test(
     subset(
-      testdt, treat == .$group1 & exclude == 0 & outcome == .$outcome
+      testdt,
+      treat == .$group1 & exclude == 0 &
+      outcome == .$outcome & male == .$male
     )$value,
     subset(
-      testdt, treat == .$group2 & exclude == 0 & outcome == .$outcome
+      testdt,
+      treat == .$group2 & exclude == 0 &
+      outcome == .$outcome & male == .$male
     )$value
   )) %>%
   summarize(
@@ -92,14 +97,20 @@ show_ttest_info <- ttest_info %>%
     outcome = .$outcome,
     group1 = .$group1,
     group2 = .$group2,
+    male = .$male,
     p = .$p,
-    y_g1 = subset(stat, outcome == .$outcome & treat == .$group1)$upr.mean,
-    y_g2 = subset(stat, outcome == .$outcome & treat == .$group2)$upr.mean
+    y_g1 = subset(
+      stat, outcome == .$outcome & treat == .$group1 & male == .$male
+    )$upr.mean,
+    y_g2 = subset(
+      stat, outcome == .$outcome & treat == .$group2 & male == .$male
+    )$upr.mean
   )) %>%
   ungroup() %>%
+  arrange(male) %>%
   mutate(
     y = if_else(y_g1 > y_g2, y_g1, y_g2) + 0.02,
-    y = if_else(id == 34, y + 0.06, y),
+    y = if_else(outcome %in% out_lab[4:6], y - 0.015, y),
     sign = case_when(
       p < 0.01 ~ "***",
       p < 0.05 ~ "**",
@@ -108,8 +119,9 @@ show_ttest_info <- ttest_info %>%
     )
   )
 
-#+ ttest-1-3step, fig.cap = "Sample Average of Outcomes before Donor Candidate Selection"
+#+ ttest-1-3step-male, fig.cap = "Average of Outcomes before Donor Candidate Selection among Males"
 stat %>%
+  dplyr::filter(male == 1) %>%
   dplyr::filter(outcome %in% out_lab[1:3]) %>%
   ggplot(aes(x = treat, y = mean)) +
     geom_bar(
@@ -126,7 +138,9 @@ stat %>%
       vjust = 3, size = 5
     ) +
     geom_signif(
-      data = show_ttest_info,
+      data = subset(
+        show_ttest_info, outcome %in% out_lab[1:3] & male == 1
+      ),
       aes(xmin = group1, xmax = group2, annotations = sign, y_position = y),
       textsize = 8, tip_length = 0.01,
       manual = TRUE
@@ -140,8 +154,9 @@ stat %>%
     ) +
     simplegg(caption_size = 13)
 
-#+ ttest-4-6step, fig.cap = "Sample Average of Outcomes after Donor Candidate Selection"
+#+ ttest-4-6step-male, fig.cap = "Average of Outcomes after Donor Candidate Selection among Males"
 stat %>%
+  dplyr::filter(male == 1) %>%
   dplyr::filter(outcome %in% out_lab[4:6]) %>%
   ggplot(aes(x = treat, y = mean)) +
     geom_bar(
@@ -157,8 +172,86 @@ stat %>%
       aes(label = sprintf("%1.3f", mean)),
       vjust = 4, size = 5
     ) +
+    geom_signif(
+      data = subset(
+        show_ttest_info, outcome %in% out_lab[4:6] & male == 1
+      ),
+      aes(xmin = group1, xmax = group2, annotations = sign, y_position = y),
+      textsize = 8, tip_length = 0.01,
+      manual = TRUE
+    ) +
     facet_wrap(~ outcome) +
-    scale_y_continuous(breaks = seq(0, 0.16, 0.02)) +
+    scale_y_continuous(breaks = seq(0, 0.2, 0.02)) +
+    labs(
+      x = "Experimental Arms",
+      y = "Sample average",
+      fill = "Experimental Arms"
+    ) +
+    simplegg()
+
+#+ ttest-1-3step-female, fig.cap = "Average of Outcomes before Donor Candidate Selection among Females"
+stat %>%
+  dplyr::filter(male == 0) %>%
+  dplyr::filter(outcome %in% out_lab[1:3]) %>%
+  ggplot(aes(x = treat, y = mean)) +
+    geom_bar(
+      color = "black",
+      fill = "grey90",
+      stat = "identity"
+    ) +
+    geom_errorbar(
+      aes(ymin = lwr.mean, ymax = upr.mean),
+      width = 0.5, position = position_dodge(0.9)
+    ) +
+    geom_text(
+      aes(label = sprintf("%1.3f", mean)),
+      vjust = 3, size = 5
+    ) +
+    geom_signif(
+      data = subset(
+        show_ttest_info, outcome %in% out_lab[1:3] & male == 0
+      ),
+      aes(xmin = group1, xmax = group2, annotations = sign, y_position = y),
+      textsize = 8, tip_length = 0.01,
+      manual = TRUE
+    ) +
+    facet_wrap(~ outcome) +
+    scale_y_continuous(breaks = seq(0, 1, 0.1)) +
+    labs(
+      x = "Experimental Arms",
+      y = "Sample average",
+      caption = "*** p < 0.01, ** p < 0.05, * p < 0.1"
+    ) +
+    simplegg(caption_size = 13)
+
+#+ ttest-4-6step-female, fig.cap = "Average of Outcomes after Donor Candidate Selection among Females"
+stat %>%
+  dplyr::filter(male == 0) %>%
+  dplyr::filter(outcome %in% out_lab[4:6]) %>%
+  ggplot(aes(x = treat, y = mean)) +
+    geom_bar(
+      color = "black",
+      fill = "grey90",
+      stat = "identity"
+    ) +
+    geom_errorbar(
+      aes(ymin = lwr.mean, ymax = upr.mean),
+      width = 0.5, position = position_dodge(0.9)
+    ) +
+    geom_text(
+      aes(label = sprintf("%1.3f", mean)),
+      vjust = 5, size = 5
+    ) +
+    geom_signif(
+      data = subset(
+        show_ttest_info, outcome %in% out_lab[4:6] & male == 0
+      ),
+      aes(xmin = group1, xmax = group2, annotations = sign, y_position = y),
+      textsize = 8, tip_length = 0.01,
+      manual = TRUE
+    ) +
+    facet_wrap(~ outcome) +
+    scale_y_continuous(breaks = seq(0, 0.2, 0.02)) +
     labs(
       x = "Experimental Arms",
       y = "Sample average",
