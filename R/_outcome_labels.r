647c54ca9ebaac5027a98722564d0b15c67b6885
KatoPachi <hkato.econ@gmail.com>
2022-07-18 16:23:23 +0900

    create source file of outcome label

---
+++ R/_outcome_labels.r
@@ -0,0 +1,13 @@
+out_lev <- c(
+  "reply", "intention", "test",
+  "candidate", "consent", "donate"
+)
+
+out_lab <- c(
+  "Reply to notification",
+  "Intention",
+  "Confirmatory typing",
+  "Candidate",
+  "Final consent",
+  "Donation"
+)
