fd6c94586de60596bec73f722227a276475f0c6e
KatoPachi <63113684+KatoPachi@users.noreply.github.com>
2023-03-16 19:15:10 +0900

    delete unused files

--- R/_outcome_labels.r
+++
@@ -1,9 +0,0 @@
-outcome_label <- list(
-  reply = "Reply",
-  positive = "Positive intention",
-  negative = "Negative intention",
-  test = "CT",
-  candidate = "Candidate",
-  consent = "Consent",
-  donate = "Donation"
-)
