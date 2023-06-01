0d2908ff8f322d002b8e6b75510c56d9e9130ee5
Hiroki Kato <hkato.econ@gmail.com>
2023-06-01 23:51:00 +0900

    create function for causal forest

---
+++ R/func/causal_forest.r
@@ -0,0 +1,109 @@
+causal_forest <- function(data) {
+  covariate <- ~ 0 + male + age + coordinate +
+    hospital_per_area + PB_per_area + BM_per_area
+  
+  usedt <- subset(data, exg_stop_reply == 0)
+
+  Y <- usedt$intention
+  X <- model.matrix(covariate, data = usedt)
+  W <- usedt$treat
+
+  int_rcf <- multi_arm_causal_forest(X, Y, W)
+  int_tau <- predict(int_rcf, X)
+
+  predict_int_rcf <- data.frame(X) %>%
+    cbind(int_tau$predictions) %>%
+    rename(
+      effect_B = `B - A.Y.1`,
+      effect_C = `C - A.Y.1`,
+      effect_D = `D - A.Y.1`
+    ) %>%
+    pivot_longer(
+      effect_B:effect_D,
+      names_to = c(".value", "treat"),
+      names_sep = "_"
+    ) %>%
+    mutate(
+      male = factor(male, labels = c("Females", "Males")),
+      treat = factor(treat, labels = paste("Message", LETTERS[2:4]))
+    )
+  
+  subset_cond <- list(
+    X[, "male"] == 0 & X[, "age"] < 30,
+    X[, "male"] == 0 & X[, "age"] >= 30,
+    X[, "male"] == 1 & X[, "age"] < 30,
+    X[, "male"] == 1 & X[, "age"] >= 30
+  )
+
+  ate_int_rcf <- subset_cond %>%
+    purrr::map(~ average_treatment_effect(int_rcf, subset = .)) %>%
+    reduce(bind_rows) %>%
+    mutate(
+      z = abs(estimate / std.err),
+      p = 2 * pnorm(z, lower.tail = FALSE),
+      gender = rep(c("Females", "Males"), each = 6),
+      age = rep(c("Young", "Elder", "Young", "Elder"), each = 3)
+    )
+
+  tbl_int_rcf <- ate_int_rcf %>%
+    select(gender, age, contrast, estimate, std.err, p) %>%
+    pivot_wider(names_from = gender, values_from = estimate:p) %>%
+    select(age, contrast, ends_with("Females"), ends_with("Males")) %>%
+    mutate(contrast = str_extract(contrast, "[B-D]")) %>%
+    mutate(age = recode(
+      age,
+      "Young" = "Age: Less than 30",
+      "Elder" = "Age: More than or equal to 30"
+    ))
+  
+  class(tbl_int_rcf) <- append(class(tbl_int_rcf), "cf_cate")
+
+  res <- list(
+    est = int_rcf,
+    pred = predict_int_rcf,
+    cate = tbl_int_rcf
+  )
+
+  res
+}
+
+cf_corr_effect <- function(rcf) {
+  est <- rcf$pred %>%
+    mutate(
+      age_less30 = if_else(age < 30, 1, 0),
+      id = rep(1:(n() / 3), each = 3)
+    ) %>%
+    pivot_wider(names_from = treat, values_from = effect) %>%
+    rename(B = `Message B`, C = `Message C`, D = `Message D`) %>%
+    group_by(male, age_less30) %>%
+    nest() %>%
+    mutate(
+      fit1 = map(
+        data,
+        ~ lm_robust(
+          D ~ I(B + C),
+          data = .,
+          se_type = "HC1"
+        )
+      ),
+      fit2 = map(
+        data,
+        ~ lm_robust(
+          D ~ B + C,
+          data = .,
+          se_type = "HC1"
+        )
+      )
+    ) %>%
+    pivot_longer(
+      fit1:fit2,
+      names_prefix = "fit",
+      names_to = "model",
+      values_to = "fit"
+    ) %>%
+    arrange(model, male, desc(age_less30)) %>%
+    pull(fit)
+  
+  class(est) <- append(class(est), "cf_corr_effect")
+  est
+}
--- R/func/tbl.r
+++ R/func/tbl.r
@@ -48,3 +48,72 @@ tbl.lm_all_stock <- function(x) {
     fontsize(size = 9, part = "all") %>%
     ft_theme()
 }
+
+tbl.cf_cate <- function(x) {
+  x %>%
+    as_grouped_data("age") %>%
+    as_flextable(hide_grouplabel = TRUE) %>%
+    set_header_labels(
+      contrast = "",
+      estimate_Females = "Estimate",
+      std.err_Females = "S.E.",
+      p_Females = "P-value",
+      estimate_Males = "Estimate",
+      std.err_Males = "S.E.",
+      p_Males = "P-value"
+    ) %>%
+    add_header_row(values = c("", "Females", "Males"), colwidths = c(1, 3, 3)) %>%
+    align(j = -1, align = "center", part = "all") %>%
+    colformat_double(j = -1, digits = 3) %>%
+    padding(j = 1:7, padding.top = 5, padding.bottom = 5, part = "all") %>%
+    padding(2:4, padding.left = 10) %>%
+    padding(6:8, padding.left = 10) %>%
+    add_footer_lines(paste(
+      "Notes: See Athey and Wager (2019) for",
+      "estimation method of conditional average treatment effect (CATE).",
+      "Since these estimates are asymptotically normal,",
+      "we calculate z-score under the null hypothesis that CATE is zero,",
+      "and obtain p-value."
+    )) %>%
+    fontsize(size = 9, part = "all") %>%
+    ft_theme()
+}
+
+tbl.cf_corr_effect <- function(x) {
+  tbl_tau_corr <- x %>%
+    setNames(paste0("(", seq_len(length(.)), ")")) %>%
+    modelsummary(
+      coef_map = c(
+        "(Intercept)" = "Constant",
+        "I(B + C)" = "B + C",
+        "B" = "B",
+        "C" = "C"
+      ),
+      statistic = c("({std.error})", "conf.int"),
+      stars = c("***" = .01, "**" = .05, "*" = .1),
+      fmt = 4,
+      gof_omit = "R2|AIC|BIC|Log|Std|FE|se_type|RMSE"
+    )
+
+  tbl_tau_corr %>%
+    add_header_row(
+      values = c("", rep(c("Age<30", "30\u2264Age", "Age<30", "30\u2264Age"), 2))
+    ) %>%
+    add_header_row(
+      values = c("", rep(c("Females", "Males"), 2)),
+      colwidths = c(1, 2, 2, 2, 2)
+    ) %>%
+    add_header_row(
+      values = c("", "Treatment effect D"),
+      colwidths = c(1, 8)
+    ) %>%
+    align(j = -1, align = "center", part = "all") %>%
+    add_footer_lines(paste(
+      "Notes: * p < 0.1, ** p < 0.05, *** p < 0.01.",
+      "Robust standard errors are reported in parentheses.",
+      "Square brackets show 95 percent confidential intervals."
+    )) %>%
+    width(j = 1, 1) %>%
+    fontsize(size = 9, part = "all") %>%
+    ft_theme()
+}
