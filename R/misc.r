ad00af8fe7df78879daa5f73cbc1cb25174356d0
Hiroki Kato <hkato.econ@gmail.com>
2023-09-26 23:48:04 +0900

    add ggplot template

--- R/misc.r
+++ R/misc.r
@@ -9,3 +9,14 @@ ft_theme <- function(ft) {
     hline_top(part = "header", border = fp_border()) %>%
     hline_top(part = "footer", border = fp_border())
 }
+
+# //NOTE template of ggplot2
+library(tidyverse)
+
+my_theme_classic <- function(size = 15) {
+    theme_classic(base_size = size) +
+    theme(
+      strip.background = element_blank(),
+      strip.text = element_text(size = size + 1, hjust = 0)
+    )
+}
