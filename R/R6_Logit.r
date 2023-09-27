b7f28c04280fe00ae2284190a6216debefe91695
Hiroki Kato <hkato.econ@gmail.com>
2023-09-27 18:06:53 +0900

    add methods of Logit class

--- R/R6_Logit.r
+++ R/R6_Logit.r
@@ -29,10 +29,36 @@ Logit <- R6::R6Class("Logit",
       cat("Regression models\n")
       for (i in 1:length(private$model)) print(private$model[[i]])
       cat("\n")
+    },
+    fit_all = function() {
+      est <- self$data %>%
+        group_by(outcome) %>%
+        nest() %>%
+        mutate(
+          fit1 = map(data, ~ glm(private$model$unctrl, data = ., family = binomial())),
+          fit2 = map(data, ~ glm(private$model$ctrl, data = ., family = binomial()))
+        ) %>%
+        ungroup() %>%
+        pivot_longer(
+          fit1:fit2,
+          names_prefix = "fit",
+          names_to = "model",
+          values_to = "fit"
+        ) %>%
+        select(-data) %>%
+        rename(covs = model) %>%
+        mutate(covs = if_else(covs == "2", "X", ""))
+
+      LogitAll$new(est)
     }
   ),
   private = list(
     ctrl_arm = "",
     model = list()
   )
+)
+
+LogitAll <- R6::R6Class("LogitAll",
+  public = list(),
+  private = list()
 )
