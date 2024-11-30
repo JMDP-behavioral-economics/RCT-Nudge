1a8d12a497c3d565e3e28d79726710b5e7ad7fe0
Hiroki Kato <hkato.econ@gmail.com>
2024-11-30 22:13:03 +0900

    add plot_density method in Flow class

--- R/R6_Flow.r
+++ R/R6_Flow.r
@@ -58,11 +58,12 @@ Flow <- R6::R6Class("Flow",
 
       private$se_type <- se
     },
-    plot = function(...,
-                    label_list,
-                    xlim = c(0, 40),
-                    ylab = "Cumulative response rate (%)",
-                    base_size = 15) {
+    plot_cumulative = function( ...,
+                                label_list,
+                                xlim = c(0, 40),
+                                ylab = "Cumulative response rate (%)",
+                                base_size = 15)
+    {
 
       dt <- self$data %>%
         select(treat, days_reply, value)
@@ -83,7 +84,7 @@ Flow <- R6::R6Class("Flow",
       }
 
       plotdt <- dt %>%
-        mutate(days_reply = if_else(value == 0, 99999, days_reply)) %>%
+        mutate(days_reply = if_else(is.na(days_reply), 99999, days_reply)) %>%
         group_by(treat, days_reply, across(starts_with("cond"))) %>%
         summarize(n = n()) %>%
         group_by(treat, across(starts_with("cond"))) %>%
@@ -123,6 +124,64 @@ Flow <- R6::R6Class("Flow",
           legend.key.size = grid::unit(1.5, "cm"),
           legend.position = "bottom"
         )
+    },
+    plot_density = function(upper_days = 99999,
+                            subset_by_gender = FALSE,
+                            subset_by_gender_age = FALSE,
+                            age_cut = 30,
+                            xlab = "Days",
+                            ylab = "Density",
+                            dlab = "Experimental arm",
+                            base_size = 15,
+                            x_breaks = seq(0, 100, by = 10))
+    {
+      if (subset_by_gender) {
+        dt <- self$data %>%
+          mutate(
+            group = male + 1,
+            group = factor(group, labels = c("Females", "Males"))
+          )
+      } else if (subset_by_gender_age) {
+        mean_age <- private$mean_age
+
+        dt <- self$data %>%
+          mutate(
+            group = case_when(
+              male == 0 & age < age_cut - mean_age ~ 1,
+              male == 0 & age >= age_cut - mean_age ~ 2,
+              male == 1 & age < age_cut - mean_age ~ 3,
+              male == 1 & age >= age_cut - mean_age ~ 4
+            ),
+            group = factor(
+              group,
+              labels = c(
+                "Young females", "Older females",
+                "Young males", "Older males"
+              )
+            )
+          )
+      } else {
+        dt <- self$data %>%
+          mutate(group = "Full sample")
+      }
+
+      dt %>%
+        filter(0 < days_reply & days_reply < upper_days) %>%
+        ggplot(aes(x = days_reply)) +
+          geom_density(aes(color = treat, linetype = treat), linewidth = .7) +
+          scale_color_manual(
+            values = c("grey30", "#00468BB2", "#ED0000B2", "#42B540B2")
+          ) +
+          scale_x_continuous(breaks = x_breaks) +
+          facet_wrap(~ group, ncol = 2, scales = "free_x") +
+          labs(
+            x = xlab,
+            y = ylab,
+            color = dlab,
+            linetype = dlab
+          ) +
+          my_theme_classic(size = base_size) +
+          theme(legend.position = "bottom")
     },
     fit = function(days, scale = 1, ...) {
       dt <- self$data
