a23dd9884f2b37b6a93a1beacb956f38a757b9ee
Hiroki Kato <hkato.econ@gmail.com>
2024-12-06 10:57:26 +0900

    Fix methods in Flow class

--- R/R6_Flow.r
+++ R/R6_Flow.r
@@ -14,66 +14,83 @@ Flow <- R6::R6Class("Flow",
       private$se_type <- se
       private$cluster <- cluster
     },
-    plot_cumulative = function( ...,
-                                label_list,
-                                xlim = c(0, 40),
+    plot_cumulative = function( upper_days,
+                                subset_by_gender = FALSE,
+                                subset_by_gender_age = FALSE,
+                                age_cut = 30,
+                                xbreaks = seq(0, 100, 5),
+                                ylim = c(0, 100),
+                                ybreaks = seq(0, 100, 10),
+                                xlab = "Days",
                                 ylab = "Cumulative response rate (%)",
+                                dlab = "Experimental Arm",
                                 base_size = 15)
     {
 
-      dt <- self$data %>%
-        select(treat, days_reply, value)
-
-      if (!missing(...)) {
-        cond <- enquos(...)
-        for (i in 1:length(cond)) {
-          col_lab <- paste0("cond", i)
-          dt[, col_lab] <- eval_tidy(cond[[i]], self$data)
-          if (!missing(label_list)) {
-            dt[, col_lab] <- factor(
-              dt[, col_lab, drop = TRUE],
-              levels = c(FALSE, TRUE),
-              labels = label_list[[i]]
+      if (subset_by_gender) {
+        dt <- self$data %>%
+          mutate(
+            group = male + 1,
+            group = factor(group, labels = c("Females", "Males"))
+          )
+      } else if (subset_by_gender_age) {
+        dt <- self$data %>%
+          mutate(
+            group = case_when(
+              male == 0 & age < age_cut ~ 1,
+              male == 0 & age >= age_cut ~ 2,
+              male == 1 & age < age_cut ~ 3,
+              male == 1 & age >= age_cut ~ 4
+            ),
+            group = factor(
+              group,
+              labels = c(
+                "Young females", "Older females",
+                "Young males", "Older males"
+              )
             )
-          }
-        }
+          )
+      } else {
+        dt <- self$data %>%
+          mutate(group = "Full sample")
       }
 
       plotdt <- dt %>%
         mutate(days_reply = if_else(is.na(days_reply), 99999, days_reply)) %>%
-        group_by(treat, days_reply, across(starts_with("cond"))) %>%
-        summarize(n = n()) %>%
-        group_by(treat, across(starts_with("cond"))) %>%
-        arrange(days_reply) %>%
-        mutate(
-          cum = cumsum(n),
-          fraq = 100 * cum / sum(n),
-          fraq = if_else(
-            days_reply == 99999,
-            100 * (cum - n) / sum(n),
-            fraq
-          )
-        )
+          group_by(treat, days_reply, group) %>%
+          summarize(
+            n = n(),
+            value = sum(value)
+          ) %>%
+          group_by(treat, group) %>%
+          arrange(days_reply) %>%
+          mutate(
+            cum_value = cumsum(value),
+            fraq = 100 * cum_value / sum(n)
+          ) %>%
+          ungroup()
 
-      out <- plotdt %>%
-        ggplot(aes(x = days_reply, y = fraq, linetype = treat, group = treat)) +
-        geom_line(linewidth = 1) +
-        scale_linetype_manual(values = c(1, 2, 4, 3)) +
-        coord_cartesian(xlim = xlim, ylim = c(0, 100)) +
-        scale_x_continuous(breaks = seq(0, 40, 5)) +
-        scale_y_continuous(breaks = seq(0, 100, 10))
-
-      if (!missing(...)) {
-        wrap_formula <- reformulate(names(dt)[str_detect(names(dt), "cond")])
-        out <- out +
-          facet_wrap(wrap_formula, scales = "free")
+      if (missing(upper_days)) {
+        xmax <- max(plotdt[plotdt$days_reply != 99999, ]$days_reply)
+      } else {
+        xmax <- upper_days
       }
 
-      out +
+      plotdt %>%
+        ggplot(aes(x = days_reply, y = fraq, linetype = treat, group = treat, color = treat)) +
+        geom_line(linewidth = 0.7) +
+        scale_color_manual(
+          values = c("grey30", "#00468BB2", "#ED0000B2", "#42B540B2")
+        ) +
+        coord_cartesian(xlim = c(0, xmax), ylim = ylim) +
+        scale_x_continuous(breaks = xbreaks) +
+        scale_y_continuous(breaks = ybreaks) +
+        facet_wrap(~ group, ncol = 2, scales = "free_x") +
         labs(
-          x = "Response speed (days)",
+          x = xlab,
           y = ylab,
-          linetype = "Treatment"
+          linetype = dlab,
+          color = dlab
         ) +
         my_theme_classic(size = base_size) +
         theme(
